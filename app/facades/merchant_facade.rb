ba73ce1bb892f78bf3f76299974c5ca1e06fe211
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-07-20 15:08:43 -0600

    Set up merchant facade

--- app/facades/merchant_facade.rb
+++ app/facades/merchant_facade.rb
@@ -1,5 +1,8 @@
 class MerchantFacade
   def self.all_merchants
-    
+    json = MerchantService.get_all_merchants
+    json[:data].map do |merchant_data|
+      Merchant.new(merchant_data)
+    end
   end
 end
